a47103b3250b610be299708b0356cc136950a869
Knut Hühne <knut@k-nut.eu>
2014-10-04 11:04:03 +0200

    add all universities

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-  $.getJSON("./htw.json", function(data){
+  $.getJSON("./tu.json", function(data){
+
+    data = _.sortBy(data, function(datum){ return datum.name;});
     _.forEach(data, function(datum){
       var $header = $("<h1>").text(datum.name[0]).appendTo("body");
-      var $text = $("<p>").text(datum.description).insertAfter($header);
+      var $link = $("<a>").text(datum.url).attr('href', datum.url).insertAfter($header);
+      var $text = $("<p>").text(datum.description).insertAfter($link);
       if (datum.dates[0]){
         var $price = $("<p>").text(datum.dates[0].price).insertAfter($text);
       }
