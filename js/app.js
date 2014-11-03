d19f0cc5f51c85f3d012ef9a98b0f972e09bb76e
Knut Hühne <knut@k-nut.eu>
2014-11-03 12:10:14 +0100

    prepare day filtering

--- js/app.js
+++ js/app.js
@@ -2,18 +2,18 @@
   "use strict";
   var app = angular.module("dashboard", []);
   app.controller("DashboardController" , function($scope, $http){
-  $scope.serachTerm = "Kicker";
+    $scope.serachTerm = "Kicker";
 
-    $http.get("http://localhost:5000/s/kicker")
-    .then(function(res){
-      $scope.sportsClasses = res.data;
-    });
+    $http.get("http://localhost:5000/s/handball")
+      .then(function(res){
+        $scope.sportsClasses = res.data;
+      });
 
-    $scope.searchClasses = function(){
-      $http.get("http://localhost:5000/s/" + $scope.searchTerm)
-        .then(function(res){
-          $scope.sportsClasses = res.data;
-        });
-    };
+      $scope.searchClasses = function(){
+        $http.get("http://localhost:5000/s/" + $scope.searchTerm)
+          .then(function(res){
+            $scope.sportsClasses = res.data;
+          });
+      };
   });
 })();
--- python/routes.py
+++ python/routes.py
@@ -9,7 +9,11 @@ def main():
 
 @app.route("/s/<query>")
 def search(query):
-    filtered_classes = [ sports_class.to_json() for sports_class in SportsClass.query.filter(SportsClass.description.contains(query))]
+    filtered_classes = [sports_class.to_json()
+                        for sports_class
+                        in SportsClass.query.filter(SportsClass.description.contains(query))
+                                            .filter(SportsClass.courses.any(day="Mi"))
+                       ]
     return json.dumps(filtered_classes)
 
 @app.route("/kro")
