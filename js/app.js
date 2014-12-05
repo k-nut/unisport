b8207002590d12127a110fae260cb95ca776953c
Knut Hühne <knut@k-nut.eu>
2014-12-05 18:22:37 +0100

    Impressum & various improvements

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 (function(){
   "use strict";
-  var app = angular.module("dashboard", []);
-  app.controller("DashboardController" , function($scope, $http){
-    $scope.serachTerm = "Kicker";
+  var app = angular.module("dashboard", ['ngSanitize']);
+  app.controller("DashboardController" , function($scope, $http, $sce){
+    $scope.searchTerm = "Handball";
     $scope.bookable = "false";
     $scope.selection = [];
     $scope.lastUpdated = "";
@@ -23,30 +23,43 @@
 
     $http.get("http://localhost:5000/s/handball")
       .then(function(res){
+        if ($scope.searchTerm){
+          _.forEach(res.data, function(datum){
+            datum.description = $sce.trustAsHtml(datum.description.replace(new RegExp("(" + $scope.searchTerm + ")", "gi"), "<span class='highlight'>$1</span>"));
+          });
+        }
         $scope.sportsClasses = res.data;
+        setTimeout(function(){$(".collapse:first").collapse()}, 500);
       });
 
-    $http.get("http://localhost:5000/age")
-      .then(function(res){
-        $scope.lastUpdated = res.data;
-      });
+      $http.get("http://localhost:5000/age")
+        .then(function(res){
+          $scope.lastUpdated = res.data;
+        });
 
-      $scope.searchClasses = function(){
-        var parameters = {};
-        if ($scope.bookable !== "false"){
-          parameters.bookable = $scope.bookable;
-        }
-        var checkedDays = _.filter($scope.days, function(day){
-          return day.checked === true;
-        }).map(function(day) { return day.day;});
-        if (checkedDays.length > 0){
-          parameters.days = checkedDays.join(",");
-        }
+        $scope.searchClasses = function(){
+          var parameters = {};
+          if ($scope.bookable !== "false"){
+            parameters.bookable = $scope.bookable;
+          }
+          var checkedDays = _.filter($scope.days, function(day){
+            return day.checked === true;
+          }).map(function(day) { return day.day;});
+          if (checkedDays.length > 0){
+            parameters.days = checkedDays.join(",");
+          }
 
+          $scope.loading = true;
           $http.get("http://localhost:5000/s/" + $scope.searchTerm, {params: parameters})
             .then(function(res){
+              if ($scope.searchTerm){
+                _.forEach(res.data, function(datum){
+                  datum.description = $sce.trustAsHtml(datum.description.replace(new RegExp("(" + $scope.searchTerm + ")", "gi"), "<span class='highlight'>$1</span>"));
+                });
+              }
               $scope.sportsClasses = res.data;
               $scope.numberOfResults = res.data.length;
+              $scope.loading = false;
             });
         };
   });
