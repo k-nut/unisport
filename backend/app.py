34694dd01fa5212b238b2c23b2cbbb686ae8b1a2
Knut Hühne <knut@k-nut.eu>
2019-11-19 07:28:46 +0100

    Disable `SQLALCHEMY_TRACK_MODIFICATIONS`
    This is hoping that it will decrease memory usage.
    It is also recommended by SQLAlchemy

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,7 @@ from backend.models import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 flask_cors.CORS(app)
 import backend.routes
