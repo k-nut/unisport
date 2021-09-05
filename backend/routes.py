7f5027c9e03192c2fa8a486ef680d1020e12fd0c
Knut Hühne <knut@k-nut.eu>
2021-09-05 13:46:49 +0200

    Expose Prometheus metrics for scraped data

--- backend/routes.py
+++ backend/routes.py
@@ -1,8 +1,20 @@
+from prometheus_client import generate_latest, Gauge, REGISTRY, PROCESS_COLLECTOR, PLATFORM_COLLECTOR
 from sqlalchemy.orm import joinedload, subqueryload, contains_eager
 
 from .app import app
-from .models import SportsClass, Course, Location
+from .models import SportsClass, Course, Location, db
 from flask import request, jsonify
+
+# Create metrics for prometheus reporting
+class_metric = Gauge('class_count', 'Class count', ['domain'])
+location_metric = Gauge('location_count', 'Location count', ['domain'])
+course_metric = Gauge('course_count', 'Course count', ['domain'])
+
+# We do not want to expose any of the metrics that the prometheus_client
+# exposes by default so we unregister them here.
+REGISTRY.unregister(PROCESS_COLLECTOR)
+REGISTRY.unregister(PLATFORM_COLLECTOR)
+REGISTRY.unregister(REGISTRY._names_to_collectors['python_gc_objects_collected_total'])
 
 
 @app.route("/names")
@@ -13,6 +25,35 @@ def names():
 @app.route("/locations")
 def locations():
     return jsonify(data=[location.to_dict() for location in Location.query.all()])
+
+
+@app.route("/metrics")
+def stats():
+    classes = db.engine.execute("""
+    SELECT array_to_string(regexp_matches(url, 'https://([\w\-.]+)/'), ';') as domain, count(*) as count
+        from class
+        group by domain
+    """).all()
+    for row in classes:
+        class_metric.labels(domain=row[0]).set(row[1])
+
+    locations = db.engine.execute("""
+         SELECT array_to_string(regexp_matches(url, 'https://([\w\-.]+)/'), ';') as domain, count(*) as count
+         from location
+         group by domain;
+    """).all()
+    for row in locations:
+        location_metric.labels(domain=row[0]).set(row[1])
+
+    courses = db.engine.execute("""
+         SELECT array_to_string(regexp_matches(sports_class_url, 'https://([\w\-.]+)/'), ';') as domain, count(*) as count
+         from course
+         group by domain;
+    """).all()
+    for row in courses:
+        course_metric.labels(domain=row[0]).set(row[1])
+
+    return generate_latest()
 
 
 @app.route("/classes", methods=["GET"])
